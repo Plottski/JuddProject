1cc42661fb99086fd8e5d9fbeb7b43953df9c447
Plottski <151260727+Plottski@users.noreply.github.com>
2024-11-13 20:17:49 -0500

    Running

--- src/main/java/com/example/JuddProject/JuddProjectApplication.java
+++ src/main/java/com/example/JuddProject/JuddProjectApplication.java
@@ -8,6 +8,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -19,22 +20,31 @@ public class JuddProjectApplication {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter the search term");
 		String searchTerm = scanner.nextLine();
+		String linkSearchTerm = searchTerm.replaceAll("\\s+","");
 		System.out.println("Please enter the number of results. Example:5, 10, 20");
 		int num = scanner.nextInt();
 		scanner.close();
 
-		String searchURL = SearchUrl + "?q="+searchTerm+"&num="+num;
+		String searchURL = SearchUrl + "?q="+linkSearchTerm+"&num="+num;
+		System.out.println(searchURL);
 
 		Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
-		System.out.println(doc.html());
+		//System.out.println(doc.html());
 
 		Elements results = doc.select("h3.r > a");
 
+		ArrayList<String> hyperLinks = new ArrayList<>();
+
 		for (Element result : results) {
 			String linkHref = result.attr("href");
+			System.out.println(linkHref);
 			String linkText = result.text();
 			System.out.println("Text::" + linkText + ", URL::" + linkHref.substring(6, linkHref.indexOf("&")));
+			hyperLinks.add(linkText);
+			System.out.println(hyperLinks);
 		}
+		System.out.println(hyperLinks);
+
 
 		SpringApplication.run(JuddProjectApplication.class, args);
 	}
