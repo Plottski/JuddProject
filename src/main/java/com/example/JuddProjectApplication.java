662b63cd02e374db4b87b035105e42068144d79c
Plottski <151260727+Plottski@users.noreply.github.com>
2024-11-15 17:28:39 -0500

    server was up and was able to ping localhost:8080

--- src/main/java/com/example/JuddProjectApplication.java
+++ src/main/java/com/example/JuddProjectApplication.java
@@ -1,11 +1,16 @@
 package com.example.JuddProject;
 
+import com.sun.tools.javac.Main;
+import jakarta.annotation.Resources;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,9 +19,69 @@ import java.util.Scanner;
 @SpringBootApplication
 public class JuddProjectApplication {
 
-	public static final String SearchUrl = "https://www.google.com/search";
+	public static void main(String[] args) {
+		SpringApplication.run(JuddProjectApplication.class, args);
+	}
+}
 
-	public static void main(String[] args) throws IOException {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public static final String SearchUrl = "https://www.google.com/search";
+
+	 throws IOException {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter the search term");
 		String searchTerm = scanner.nextLine();
@@ -35,18 +100,23 @@ public class JuddProjectApplication {
 
 		ArrayList<String> hyperLinks = new ArrayList<>();
 
+
+
 		for (Element result : results) {
+			System.out.println(result);
 			String linkHref = result.attr("href");
 			System.out.println(linkHref);
 			String linkText = result.text();
 			System.out.println("Text::" + linkText + ", URL::" + linkHref.substring(6, linkHref.indexOf("&")));
 			hyperLinks.add(linkText);
 			System.out.println(hyperLinks);
+			String daLink = linkHref;
+			System.out.println(daLink);
+			hyperLinks.add(daLink);
 		}
 		System.out.println(hyperLinks);
 
 
 		SpringApplication.run(JuddProjectApplication.class, args);
 	}
-
-}
+*/
