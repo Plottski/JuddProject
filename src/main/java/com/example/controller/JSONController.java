12ed2173b9c2d1d75c769206be0aa27ea71feb37
justinvail <justinmvail@gmail.com>
2024-11-18 18:29:22 -0500

    Light clean up and refactoring of JSON Controller to improve readability and logging.

--- src/main/java/com/example/controller/JSONController.java
+++ src/main/java/com/example/controller/JSONController.java
@@ -1,17 +1,5 @@
 package com.example.controller;
-import com.example.JuddProject.FormData;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import jakarta.servlet.http.HttpSession;
-import org.apache.tomcat.util.json.JSONParser;
-import org.jsoup.Jsoup;
-import org.springframework.boot.json.JacksonJsonParser;
-import org.springframework.boot.json.JsonParseException;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.io.BufferedReader;
@@ -19,72 +7,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 public class JSONController {
-
     String apiKey = "AIzaSyDiYoriL2BqPxdkCChUjV6Y1TeLGhsibns";
-
     String cxID = "233f0749f131242a6";
 
     public static String encodeValue(String value) throws UnsupportedEncodingException {
-
         return URLEncoder.encode(value, StandardCharsets.UTF_8);
     }
 
     @RequestMapping(path = "/search", method = RequestMethod.POST)
-    //@PostMapping("/search", method = RequestMethod.POST)
-    //public ResponseEntity<String> handleFormData(HttpSession session, @RequestBody FormData theData) throws JsonProcessingException {
-    //public ResponseEntity<String> handleFormData(HttpSession session, @RequestBody HashMap<String,String> whatJuddWant) {
     public ResponseEntity<String> handleFormData(HttpSession session, @RequestBody String theData) throws IOException {
-
-        System.out.println(theData);
-      /*  HashMap<String,String> json = new HashMap<>();
-        HashMap mapper;
-        JacksonJsonParser parser = new JacksonJsonParser();
-        String payload = whatJuddWant.getParameters();
-        mapper = (HashMap) parser.parseMap(payload);
-        System.out.println(payload);
-*/
-        ObjectMapper objectMapper = new ObjectMapper();
-        //FormData isThisWhatJuddWants = objectMapper.
-        //FormData newData = objectMapper.readValue(theData,FormData.class);
-        //String json = theData.getParameters();
-        //System.out.println(newData);
-        //System.out.println(json);
-        //System.out.println(theData);
-        //System.out.println("Parameters = " + theData.getParameters());
-        //System.out.println(whatJuddWant);
-        //System.out.println("Paramaters - " + whatJuddWant.getParameters());
-        //URLEncoder urlEncoder
-
+        System.out.println("incoming String: " + theData);
         String searchQuery = encodeValue(theData);
-        System.out.println(searchQuery);
+        System.out.println("searchQuery: " + searchQuery);
 
+//        GET https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
         URL url = new URL("https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + cxID + "&q=" + searchQuery);
-
         String urlString = url.toString();
         Object parser = url.getContent();
 
-        System.out.println(parser);
-
-        System.out.println(url);
-        System.out.println(urlString);
+        System.out.println("parser: "+parser);
+        System.out.println("url: " + url);
+        System.out.println("urlString" + urlString);
 
         HttpURLConnection con;
         con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
 
         int responseCode = con.getResponseCode();
-        String googleResponse = new String();
+        String googleResponse = null;
 
         if (responseCode == 200) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
@@ -94,29 +50,13 @@ public class JSONController {
                 response.append(line);
             }
             reader.close();
-
-            System.out.println(response);
+            System.out.println("response: "+ response);
             googleResponse = response.toString();
-
             System.out.println(googleResponse);
-
-            ObjectMapper mapper = new ObjectMapper();
-
-
-        }
-        else {
+        } else {
             System.out.println("You done goofed son!");
         }
-
-
         return ResponseEntity.ok(googleResponse);
-
-
-        //This is an example of the URL format that needs to be done to perform a get request
-        //GET https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
-
-
-        //return ResponseEntity.ok("Form data received successfully!");
     }
 }
 
