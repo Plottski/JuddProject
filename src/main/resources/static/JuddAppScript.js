5393d8e94c03d33d88eac05f8b284e2c81fff952
justinvail <justinmvail@gmail.com>
2024-11-19 16:44:38 -0500

    Refactor of JuddAppScript.js.  Also cleaned up logging.  Fixed problems that were caused be htmlform by simply removing that paradigm.

--- src/main/resources/static/JuddAppScript.js
+++ src/main/resources/static/JuddAppScript.js
@@ -1,8 +1,3 @@
-//mainContainer.innerHTML="";
-  //theButton.addEventListener("onclick", spaFunction)
-  function myFunction() {
-      console.log("Button clicked!")
-  }
 
   window.onload = function() {
     const mainContainer = document.getElementById("mainContainer");
@@ -11,92 +6,51 @@
   }
 
   function spaFunction() {
-    console.log("Success");
+    console.log("spaFunction()...  Deleting existing elements");
     var gSearchBar = document.getElementById("googleSearch");
     var theButton = document.getElementById("spaEnabler");
     gSearchBar.parentNode.removeChild(googleSearch);
     theButton.parentNode.removeChild(theButton);
+    console.log("spaFunction()...  elements deleted");
 
-    var juddForm = document.createElement("form");
-    juddForm.id = "formForJudd";
-
-    var juddWant = document.createElement("input");
-    juddWant.type = "submit";
-    juddWant.id = "juddGet";
-    juddWant.placeholder = "Judd Get!!!!";
-
+    console.log("spaFunction()...  creating new elements");
+    var button = document.createElement("button");
+    button.type = "button";
+    button.id = "button";
+    button.innerHTML = "button text";
     var juddSearchBar = document.createElement("input");
     juddSearchBar.type = "text";
     juddSearchBar.id = "juddWantDis";
     juddSearchBar.placeholder = "What Judd Want?????";
+    mainContainer.append(juddSearchBar);
+    mainContainer.append(button);
+    console.log("spaFunction()...  elements created");
 
-
-    juddForm.append(juddSearchBar);
-    juddForm.append(juddWant);
-
-    mainContainer.append(juddForm);
-
-    juddForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-
-          $('#formForJudd').submit(function(event) {
-            console.log(whatJuddWant);
-            //event.preventDefault();
-            var whatJuddWant = $("#juddWantDis").val();
-            //var whatJuddWant = {
-              //juddWantStuff: $('#juddWantDis').val()
-          //  }
-            console.log(whatJuddWant);
-
-
-
-            $.ajax({
-              url: '/search',
-              type: 'POST',
-              contentType: 'application/json',
-              //data:whatJuddWant.val(),
-              //data: JSON.stringify(whatJuddWant),
-              //data: {
-                //theData:whatJuddWant
-              //},
-              //data: JSON.stringify({
-                //theData:whatJuddWant
-             // }),
-             data:whatJuddWant,
-              Success: function(data) {
-                var msg = JSON.parse(data);
-                console.log(msg);
-                doThings(msg);
-                //doThings(data);
-                //console.log(handleFormData);
-                //var serverData = []
-                //serverData = JSON.parse(respons);
-                //console.log("Success:", serverData);
-                //return serverData;
-                //if response.status === 200 {
-                  //var data = response;
-                  //var parsedJson = JSON.parse(data);
-                //  console.log(data);
-                  //console.log(parsedJson);
-              },
-              error: function(error) {
-                console.error("Error:", error);
-              }
-            });
-          });
-    });
-
-    $(document).ajaxComplete(function(serverData) {
-      doThings(serverData);
-    });
-
-  //  $.post('/search', function(data) {
-    //  var serverData = [];
-      //  serverData = data;
-      //  console.log(data);
-    //});
+      $('#button').click(function() {
+          var whatJuddWant = $("#juddWantDis").val();
+          callServer(whatJuddWant)
+      });
   }
 
-  function doThings(msg) {
-    console.log(msg);
-  }
+function callServer(whatJuddWant) {
+    console.log("spaFunction()...  Deleting existing elements");
+    console.log("callServer(): "+whatJuddWant);
+
+    $.ajax({
+        url: "/search",
+        method: "POST",
+        data: whatJuddWant,
+        success: function(data) {
+            var msg = JSON.parse(data);
+            console.log(msg);
+            const brElement = document.createElement("br");
+            for(var i = 0; i<msg.items.length; i++){
+                mainContainer.append(msg.items[i].title);
+                mainContainer.append(msg.items[i].htmlTitle);
+            }
+        },
+        error : function(request,error){
+            alert("Request: "+request);
+        }
+    });
+}
